d4237b9f0958eef3ac3134e55569700082fbeb1c
Leonardo Larrañaga <luis.larranaga@uabc.edu.mx>
2025-01-18 16:00:41 -0800

    Initial Commit. Sidebar Position.

---
+++ Browser/Settings View.swift
@@ -0,0 +1,18 @@
+//
+//  Settings View.swift
+//  Browser
+//
+//  Created by Leonardo Larrañaga on 1/18/25.
+//
+
+import SwiftUI
+
+struct Settings_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settings_View()
+}
---
+++ Browser/UserPreferences.swift
@@ -0,0 +1,8 @@
+//
+//  UserPreferences.swift
+//  Browser
+//
+//  Created by Leonardo Larrañaga on 1/18/25.
+//
+
+import Foundation
