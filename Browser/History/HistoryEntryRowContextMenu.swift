de1d46a3d14da8e5b5a51124ae6b860f28b1030a
Leonardo Larrañaga <luis.larranaga@uabc.edu.mx>
2025-03-01 16:03:34 -0800

    Add remaining translation languages and fix encoded symbols

---
+++ Browser/History/HistoryEntryRowContextMenu.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryEntryRowContextMenu.swift
+//  Browser
+//
+//  Created by Leonardo Larrañaga on 3/1/25.
+//
+
+import SwiftUI
+
+struct HistoryEntryRowContextMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryEntryRowContextMenu()
+}
